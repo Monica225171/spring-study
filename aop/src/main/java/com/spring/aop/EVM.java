ecb99b4548de28af4792e3c700ed9d9d57186f56
Monica225171 <2251712358@qq.com>
2019-03-11 20:08:33 +0800

    ORM Study

--- aop/src/main/java/com/spring/aop/EVM.java
+++ aop/src/main/java/com/spring/aop/EVM.java
@@ -12,12 +12,16 @@ public class EVM {
         QrConfig config = new QrConfig(300, 300);
         // 设置边距，既二维码和背景之间的边距
         config.setMargin(3);
-        // 设置前景色，既二维码颜色（青色）
-        config.setForeColor(Color.CYAN.getRGB());
-        // 设置背景色（灰色）
-        config.setBackColor(Color.GRAY.getRGB());
+        // 设置前景色，既二维码颜色
+        Color foreColor=new Color ( 58 ,95 ,205);
+        // 设置背景色
+        Color bgColor=new Color ( 255 ,193, 37 );
+        int foreColorRGB=foreColor.getRGB ();
+        int bgColorRGB=bgColor.getRGB ();
+        config.setForeColor(foreColorRGB);
+        config.setBackColor(bgColorRGB);
         //附带logo
         config.setImg ("d:/img.jpg");
-        QrCodeUtil.generate("https://www.jianshu.com/u/1eb9e2dd4cdc", config,FileUtil.file("d:/EVM.jpg"));
+        QrCodeUtil.generate("https://www.jianshu.com/u/1eb9e2dd4cdc", config ,FileUtil.file("d:/EVM.jpg"));
         }
 }
